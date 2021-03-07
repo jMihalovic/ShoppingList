eee926f0f623e02df46295a2d9542a7c7537a10b
Jakub Mihalovič <mihalovic.jakub.2018@skola.ssps.cz>
2021-03-07 18:32:22 +0100

    Přidejte soubory projektu.

---
+++ ShoppingList/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace ShoppingList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButPlus = new System.Windows.Forms.Button();
+            this.NakupLabel = new System.Windows.Forms.Label();
+            this.HotovoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ButPlus
+            // 
+            this.ButPlus.Location = new System.Drawing.Point(12, 100);
+            this.ButPlus.Name = "ButPlus";
+            this.ButPlus.Size = new System.Drawing.Size(23, 23);
+            this.ButPlus.TabIndex = 0;
+            this.ButPlus.Text = "+";
+            this.ButPlus.UseVisualStyleBackColor = true;
+            this.ButPlus.Click += new System.EventHandler(this.ButPlus_Click);
+            // 
+            // NakupLabel
+            // 
+            this.NakupLabel.AutoSize = true;
+            this.NakupLabel.Location = new System.Drawing.Point(53, 73);
+            this.NakupLabel.Name = "NakupLabel";
+            this.NakupLabel.Size = new System.Drawing.Size(52, 13);
+            this.NakupLabel.TabIndex = 1;
+            this.NakupLabel.Text = "Nakup si:";
+            // 
+            // HotovoLabel
+            // 
+            this.HotovoLabel.AutoSize = true;
+            this.HotovoLabel.Location = new System.Drawing.Point(484, 73);
+            this.HotovoLabel.Name = "HotovoLabel";
+            this.HotovoLabel.Size = new System.Drawing.Size(42, 13);
+            this.HotovoLabel.TabIndex = 2;
+            this.HotovoLabel.Text = "Hotovo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 450);
+            this.Controls.Add(this.HotovoLabel);
+            this.Controls.Add(this.NakupLabel);
+            this.Controls.Add(this.ButPlus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButPlus;
+        private System.Windows.Forms.Label NakupLabel;
+        private System.Windows.Forms.Label HotovoLabel;
+    }
+}
+
---
+++ ShoppingList/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShoppingList
+{
+    public partial class Form1 : Form
+    {
+        List<Panel> List = new List<Panel>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButPlus_Click(object sender, EventArgs e)
+        {
+            Panel panel = new Panel()
+            {
+                Size = new Size(562,32)
+            };
+            TextBox text = new TextBox()
+            {
+                Parent = panel,
+                Size = new Size(415,20),
+                Location = new Point(3,3)
+            };
+            CheckBox check = new CheckBox()
+            {
+                Parent = panel,
+                Size = new Size(15,14),
+                Location = new Point(442, 3)
+            };
+            Button but = new Button()
+            {
+                Parent = panel,
+                Size = new Size(75, 23),
+                Location = new Point(484,3),  
+                Text = "Smazat"
+            };
+            but.Click += new System.EventHandler(this.ButSmazat_Click);
+            Controls.Add(panel);
+            List.Add(panel);
+            Serazeni();
+        }
+
+        private void ButSmazat_Click(object sender, EventArgs e)
+        {
+            Button butt = (Button)sender;
+            Panel pan = (Panel)butt.Parent;
+            Controls.Remove(pan);
+            List.Remove(pan);
+            Serazeni();
+        }
+
+        private void Serazeni()
+        {
+            foreach (Control x in this.Controls)
+            {
+                if(x.GetType().Name == "Panel")
+                {
+                    x.Location = new Point(56, 100 + List.IndexOf((Panel)x)*32);
+                }
+            }
+        }
+    }
+}
